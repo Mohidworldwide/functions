09e049dc591a0a546e12475b0b5541239d84d844
Mohidworldwide <mohidssharif@gmail.com>
2024-10-13 20:50:07 +0500

    functions in python

---
+++ main.py
@@ -0,0 +1,47 @@
+# # # #activity 1
+
+# # # # def printname(name):
+# # # #     print ("my name is,name")
+# # # # n=input("enter your name")
+# # # # printname (n)
+
+# # # def printhobby(hobby):
+# # #     print ("my hobby is,hobby")
+# # # h=input ("enter your hobby")
+# # # printhobby (h)
+
+# # #activity 2
+# # def add(x,y):
+# #     return x+y
+# # def subtract(x,y):
+# #     return x-y
+# # def divide(x,y):
+# #     return x/y
+# # def multiply (x,y):
+# #     return x*y
+
+# # num1=int(input("enter number 1 ======>"))
+# # num2=int(input("enter number 2 ======>"))
+
+# # print ("addition",add(num1,num2))
+# # print ("subtraction",subtract(num1,num2))
+# # print ("division",divide(num1,num2))
+# # print ("multiplication",multiply(num1,num2))
+
+# #activity 3
+
+# num = int(input("enter the number to check if it is positive or negative"))
+# def positivenegative (x):
+#         if x>0:
+#             print ("it is positive",x)
+#         else:
+#               print("it is negative",x)
+
+
+
+
+# positivenegative(num)
+
+
+
+
